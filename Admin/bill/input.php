92b37fc6829fe177482624201278fae5369464c7
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-01-15 23:21:12 -0800

    add feature laporan

---
+++ Admin/bill/input.php
@@ -0,0 +1,12 @@
+<?php
+    include "../../Koneksi/koneksi.php";
+    include "../../Data/insert_awb.php";
+
+    if($sql){
+            echo '<script language="javascript">alert("Data berhasil diproses!")</script>';
+            echo '<script>window.location="price.php"</script>';
+    }
+    else {
+        echo mysqli_error($conn);
+        echo "gagal proses!";
+    }
